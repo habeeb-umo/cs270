54d5355af0800ab9a6047ca13d54d5295f983db7
Habeeb Mohammed <habeeb@atlanta.cs.colostate.edu>
2016-08-24 00:09:58 -0600

    P1 complete

--- P1/P1.c
+++ P1/P1.c
@@ -1,42 +1,96 @@
 // P1 Assignment
-// Author: Chris Wilcox
+// Author: Habeeb Mohammed
 // Date:   1/8/2016
 // Class:  CS270
-// Email:  wilcox@cs.colostate.edu
+// Email:  habeeb@cs.colostate.edu
 
 // Include files
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void computeSphere(double radius, double *addressOfVolume)
-{
-    // Compute volume
-    double result = (4.0 / 3.0) * (3.141593 * radius * radius * radius);
+static double input[4];
+static double output[4];
 
-    // Dereference pointer to return result
-    *addressOfVolume = result;
+void computeCircle(double radius, double *addressOfArea)
+{
+	
+	//Compute area
+	double result = 3.141593 * (radius * radius);
+
+	//Dereference pointer to return result
+	*addressOfArea = result;
+}
+
+void computeTriangle(double side, double *addressOfArea)
+{
+	
+	//Compute area
+	double result = .433013 * (side * side);
+
+	//Dereference pointer to return result
+	*addressOfArea = result;
+}
+
+void computeSquare(double side, double *addressOfArea)
+{
+	
+	//Compute area
+	double result = side * side;
+
+	//Dereference pointer to return result
+	*addressOfArea = result;
+}
+
+void computePentagon(double side, double *addressOfArea)
+{
+	
+	//Compute area
+	double result = 1.720477 * (side * side);
+
+	//Dereference pointer to return result
+	*addressOfArea = result;
 }
 
 int main(int argc, char *argv[])
 {
+	
+	//double radiusCircle = 0.0;
+	//double sideTriangle = 0.0;
+	//double sideSquare = 0.0;
+	//double sidePentagon = 0.0;
+
     // Check number of arguments
-    if (argc != 2) {
-        printf("usage: ./P1 <double>\n");
+    if (argc != 5) 
+    {	printf("%d\n",argc);
+        printf("usage: ./P1 <double> <double> <double> <double>\n");
         return EXIT_FAILURE;
     }
+	// Parse arguments
+		input[0] = atof(argv[1]);
+		input[1] = atof(argv[2]);
+		input[2] = atof(argv[3]);
+		input[3] = atof(argv[4]);
 
-    // Parse arguments
-    double radius = atof(argv[1]);
-    
-    // Local variable
-    double volume;
+	// Local variables
+		//double areaCircle;
+		//double areaTriangle;
+		//double areaSquare;
+		//double areaPentagon;
 
-    // Call function
-    computeSphere(radius, &volume);
-    
+
+    // Call functions
+    	computeCircle(input[0], &output[0]);
+	computeTriangle(input[1], &output[1]);
+	computeSquare(input[2], &output[2]);
+	computePentagon(input[3], &output[3]);
+
     // Print volume
-    printf("The volume of a sphere with radius %.5f equals %.5f.\n", radius, volume);
+	printf("CIRCLE, radius = %.5f, area = %.5f.\n", input[0], output[0]);
+	printf("TRIANGLE, length = %.5f, area = %.5f.\n", input[1], output[1]);
+	printf("SQUARE, length = %.5f, area = %.5f.\n", input[2], output[2]);
+	printf("PENTAGON, length = %.5f, area = %.5f.\n", input[3], output[3]);
+
 
     // Return success
     return EXIT_SUCCESS;
