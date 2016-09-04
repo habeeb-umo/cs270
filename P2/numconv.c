17d3463f63c20297f3d01a471862b7d7b9dee4e6
Habeeb Mohammed <habeeb@atlanta.cs.colostate.edu>
2016-09-04 00:55:21 -0600

    finished P2 (e/c still todo)

--- P2/numconv.c
+++ P2/numconv.c
@@ -5,12 +5,16 @@
  *  habeeb@cs.colostate.edu
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 /** @todo implement in <code>numconv.c</code> based on documentation contained 
  *  in <code>numconv.h</code>.
  */
 char int2char (int radix, int digit) {
+
 	char cFinal;
-	// radix cannot equal number and radix must be between 2 and 36
+
+	// radix be less than number and radix must be between 2 and 36
 	if(digit >= radix || radix < 2 || radix > 36) 
 	{
 		cFinal = '?';
@@ -40,8 +44,10 @@ char int2char (int radix, int digit) {
  *  in <code>numconv.h</code>.
  */
 int char2int (int radix, char digit) {
+
   	int numFinal = -1;
-	// radix cannot equal number and radix must be between 2 and 36
+
+	// radix must be between 2 and 36
 	if(radix < 2 || radix > 36) 
 	{
 		return -1;
@@ -89,25 +95,71 @@ int char2int (int radix, char digit) {
  *  in <code>numconv.h</code>.
  */
 void divRem (int numerator, int divisor, int* quotient, int* remainder) {
+
+	int rFinal;
+	int qFinal;
+
+	// Quotient and modulo formulas
+	rFinal = numerator % divisor;
+	qFinal = numerator / divisor;
+
+	// Return via pointers
+	*remainder = rFinal;
+	*quotient = qFinal;
 }
 
 /** @todo implement in <code>numconv.c</code> based on documentation contained 
  *  in <code>numconv.h</code>.
  */
 int ascii2int (int radix, const char *str) {
-  return -1;
+
+	int numFinal;
+	char *endptr;
+
+	// radix must be between 2 and 36
+	if(radix < 2 || radix > 36)
+	{
+		return -1;
+	}
+	else
+	// strtol(str, &endptr, base) is string to long int
+	numFinal = strtol(str, &endptr, radix);
+
+  return numFinal;
 }
 
 /** @todo implement in <code>numconv.c</code> based on documentation contained 
  *  in <code>numconv.h</code>.
  */
 void int2ascii (int radix, int value) {
+	
+	// base case
+	if(value <= 0){
+
+	}
+	// recursive case
+	else{
+
+		int2ascii(radix, value / radix);
+		putchar(int2char(radix,value % radix));
+	}
 }
 
 /** @todo implement in <code>numconv.c</code> based on documentation contained 
  *  in <code>numconv.h</code>.
  */
 double ascii2double (int radix, const char *str) {
-  return -1.0;
+	
+	double dFinal;
+	char *endPtr;
+	
+	if(radix < 2 || radix > 36)
+	{
+		return -1.0;
+	}
+  	else
+	dFinal = strtod(str, &endPtr);
+
+return dFinal;
 }
 
