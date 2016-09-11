2c254499dfbe95bf5f1a10dbe0f0b05972e1eb21
Habeeb Mohammed <habeeb.umo@gmail.com>
2016-09-11 01:52:41 -0600

    P3 in progress (checkin test)

--- P3/flt32.c
+++ P3/flt32.c
@@ -1,5 +1,6 @@
 #include "flt32.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /** @file flt32.c
  *  @brief You will modify this file and implement nine functions
  *  @details Your implementation of the functions defined in flt32.h.
@@ -13,23 +14,37 @@
 void printBinary (int value);
 
 /** @todo Implement in flt32.c based on documentation contained in flt32.h */
-int flt32_get_sign (flt32 x) {
+int flt32_get_sign (flt32 x){
 
-  return 0; /* implement this */
+	return x >> 31 & 1; /* implement this */
 }
 
 /** @todo Implement in flt32.c based on documentation contained in flt32.h */
 int flt32_get_exp (flt32 x) {
-  return 0; /* implement this */
+	x = x >> 23;
+	int field = (8 - 0) + 1;
+	int mask = (1 << field) - 1;
+	printBinary(x & mask);
+	printf("\n");
+  
+	return x & mask; /* implement this */
 }
 
 /** @todo Implement in flt32.c based on documentation contained in flt32.h */
 int flt32_get_val (flt32 x) {
-  return 0;
+	int field = (23 - 0) + 1;
+	int mask = (1 << field) - 1;
+	printBinary(x & mask);
+	printf("\n");
+
+  	return x & mask;
 }
 
 /** @todo Implement in flt32.c based on documentation contained in flt32.h */
 void flt32_get_all(flt32 x, int* sign, int*exp, int* val) {
+	*sign = flt32_get_sign(x); 
+	*exp = flt32_get_exp(x);
+	*val = flt32_get_val(x);
 }
 
 /** @todo Implement in flt32.c based on documentation contained in flt32.h */
