4c8de5ebe05420b3bc68a9e3b69871350c9a66d4
habeeb <habeeb.umo@gmail.com>
2016-09-11 03:11:27 -0600

    All prelims cleared

--- P3/flt32.c
+++ P3/flt32.c
@@ -13,32 +13,33 @@
 
 void printBinary (int value);
 
-/** @todo Implement in flt32.c based on documentation contained in flt32.h */
 int flt32_get_sign (flt32 x){
-
-	return x >> 31 & 1; /* implement this */
+	
+	// 31 & 1 clears (1,31) leaving only sign bit
+	return x >> 31 & 1; 
 }
 
-/** @todo Implement in flt32.c based on documentation contained in flt32.h */
 int flt32_get_exp (flt32 x) {
+
 	x = x >> 23;
 	int field = (8 - 0) + 1;
 	int mask = (1 << field) - 1;
   
-	return x & mask; /* implement this */
+	return x & mask;
 }
 
-/** @todo Implement in flt32.c based on documentation contained in flt32.h */
 int flt32_get_val (flt32 x) {
-
+	
+	// Mantissa = (0,23)
 	int field = (22 - 0) + 1;
 	int mask = (1 << field) - 1;
-	
+
+	// 8388608 = implicit 1;	
   	return (x & mask) + 8388608;
 }
 
-/** @todo Implement in flt32.c based on documentation contained in flt32.h */
 void flt32_get_all(flt32 x, int* sign, int*exp, int* val) {
+
 	*sign = flt32_get_sign(x); 
 	*exp = flt32_get_exp(x);
 	*val = flt32_get_val(x);
